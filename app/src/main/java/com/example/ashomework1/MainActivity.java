5c428800a60b055c6d926d5ffee16e95d297cdea
nur4ik-03 <4u.lp@bk.ru>
2020-04-01 22:02:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/ashomework1/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.ashomework1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView number_day = (TextView) findViewById(R.id.number_day);
+        TextView LHR = (TextView) findViewById(R.id.LHR_);
+        TextView lond = (TextView) findViewById(R.id.lond);
+        ImageView fle = (ImageView) findViewById(R.id.flyy);
+        TextView nam = (TextView) findViewById(R.id.nam);
+        TextView name = (TextView) findViewById(R.id.name);
+        TextView fli = (TextView) findViewById(R.id.fli);
+        TextView va = (TextView) findViewById(R.id.va);
+        TextView sea = (TextView) findViewById(R.id.sea);
+        TextView toa = (TextView) findViewById(R.id.toa);
+        TextView depa = (TextView) findViewById(R.id.depa);
+        TextView five = (TextView) findViewById(R.id.five);
+        TextView arriv = (TextView) findViewById(R.id.arriv);
+        TextView nine = (TextView) findViewById(R.id.nine);
+        ImageView shtr = (ImageView) findViewById(R.id.shtr);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
